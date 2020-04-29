f6031fa522f7a4b2bf518530280167c9caba92bd
Christoffer Lybekk <christoffer@lybekk.tech>
2020-04-29 19:36:58 +0200

    PWA(Progressive web app) support added

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,21 @@ module.exports = {
         "artifactName": "${productName}_desktop.${ext}"
       }
     }
+  },
+  pwa: {
+    name: 'pimpim',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    //workboxPluginMode: 'InjectManifest',
+    workboxPluginMode: 'GenerateSW',
+    /* no need for InjectManifest yet. 
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+    }
+    */
   }
 }
