d1df0f228d295ba5a2495f6051d2db64f26f53c1
Christoffer Lybekk <christoffer@lybekk.tech>
2020-07-11 15:11:57 +0200

    prettier whiteboard

--- src/components/app/LocalWhiteboard.vue
+++ src/components/app/LocalWhiteboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" width="45vw" scrollable>
+  <v-dialog v-model="dialog" width="45vw" :fullscreen="isSmallScreen" scrollable>
     <template v-slot:activator="{ on, attrs }">
       <v-btn icon v-bind="attrs" v-on="on">
         <v-icon>mdi-clipboard</v-icon>
@@ -8,22 +8,27 @@
     <v-card>
       <v-banner v-model="banner">Whiteboard will not be synced, but stored locally in the browser, and retained across sessions.</v-banner>
       <v-card-text>
-        <v-textarea v-model="content" class="text-body-2" rows="6" auto-grow spellcheck="false" />
+        <v-textarea v-model="content" auto-grow spellcheck="false" hide-details autofocus />
       </v-card-text>
       <v-card-actions>
         <v-btn icon color="secondary" @click="banner = !banner">
           <v-icon>mdi-information</v-icon>
         </v-btn>
         <v-spacer />
-      <span class="primary--text subtitle-1">Whiteboard</span>
+              <v-btn text color="secondary" @click="dialog = !dialog">
+                Close
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import formatMixin from '@/mixins/formatMixin'
+
 export default {
   name: "LocalWhiteboard",
+  mixins: [formatMixin],
   data: () => ({
     dialog: false,
     banner: false,
