901179d53e360782f3a8d0888e21bcdc5614226c
Christoffer Lybekk <christoffer@lybekk.tech>
2020-08-20 02:05:35 +0200

    Fix: Notes extended viewport. Added ellipsis.

--- src/components/notes/NotesItem.vue
+++ src/components/notes/NotesItem.vue
@@ -1,61 +1,77 @@
-<template lang="pug">
-  div
-    v-hover(v-slot:default="{ hover }")
-      v-card(
-        hover 
-        ripple 
-        :disabled="isDeleted(doc._id)" 
-        :flat="isDeleted(doc._id)"
-      )
-        v-card-title(
-          class="subheading" 
-          @click="showItemDetailed"
-        ) {{ doc.title }}
-        v-card-subtitle(
-          @click="showItemDetailed"
-        ) {{ doc.description }}
-        v-card-actions
-          v-spacer
-          v-fab-transition
-            v-speed-dial(
-              absolute 
-              bottom 
-              right
-            )
-              template(v-slot:activator)
-                v-btn(
-                  v-show="hover" 
-                  icon 
-                  small 
-                  fab
-                )
-                  v-icon mdi-delete
-              v-btn(
-                v-show="hover" 
-                fab
+<template>
+  <v-hover v-slot:default="{ hover }">
+    <v-card
+      hover
+      ripple
+      :disabled="isDeleted(doc._id)"
+      :flat="isDeleted(doc._id)"
+    >
+      <v-card-title
+        @click="showItemDetailed"
+      >
+        {{ docTitle }}
+      </v-card-title>
+      <v-card-text @click="showItemDetailed">
+        {{ docDescription }}
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-fab-transition>
+          <v-speed-dial
+            absolute
+            bottom
+            right
+          >
+            <template v-slot:activator>
+              <v-btn
+                v-show="hover"
+                icon
                 small
-                color="yellow lighten-2"
-                @click="deleteDoc(doc._id)"
-              )
-                v-icon(color="black") mdi-radioactive
-
-        v-fade-transition
-          v-overlay(
-            v-if="isDeleted(doc._id)"
-            absolute
-            color="error"
-            z-index="3"
-          )
-            v-btn(text) Deleted
-        v-fade-transition
-          v-overlay(
-            v-if="doc.archived"
-            absolute
-            color="info"
-            z-index="3"
-          )
-            v-btn(text) Archived
-
+                fab
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </template>
+            <v-btn
+              v-show="hover"
+              fab
+              small
+              color="yellow lighten-2"
+              @click="deleteDoc(doc._id)"
+            >
+              <v-icon color="black">
+                mdi-radioactive
+              </v-icon>
+            </v-btn>
+          </v-speed-dial>
+        </v-fab-transition>
+      </v-card-actions>
+      <v-fade-transition>
+        <v-overlay
+          v-if="isDeleted(doc._id)"
+          absolute
+          color="error"
+          z-index="3"
+        >
+          <v-btn text>
+            Deleted
+          </v-btn>
+        </v-overlay>
+      </v-fade-transition>
+      <v-fade-transition>
+        <v-overlay
+          v-if="doc.archived"
+          absolute
+          color="info"
+          z-index="3"
+        >
+          <v-btn text>
+            Archived
+          </v-btn>
+        </v-overlay>
+      </v-fade-transition>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
@@ -69,7 +85,13 @@ export default {
     pouchMixin,
     itemMixin
     ],
-  props: ["doc"],
+  //props: ["doc"],
+  props: {
+    doc: {
+      type: Object,
+      default: () => {},
+    }
+  },
   data: () => ({
     trashIconClicked: false,
     itemsPerPage: 8
@@ -80,6 +102,22 @@ export default {
         return true;
       }
       return false;
+    },
+    docTitle() {
+      let t = this.doc.title;
+      if (t.length > 60) {
+        return t.slice(0, 60) + '...'
+      } else {
+        return t
+      }
+    },
+    docDescription() {
+      let t = this.doc.description;
+      if (t.length > 120) {
+        return t.slice(0, 120) + '...'
+      } else {
+        return t
+      }
     }
   },
   methods: {
