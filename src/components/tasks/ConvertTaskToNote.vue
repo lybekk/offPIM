28afa14af0357fa6dda25c1f8a2a440e005762c0
Christoffer Lybekk <christoffer@lybekk.tech>
2020-08-23 21:12:37 +0200

    Feature: convert task to note

---
+++ src/components/tasks/ConvertTaskToNote.vue
@@ -0,0 +1,40 @@
+<template>    
+  <v-list-item
+    @click="convert()"
+  >
+    <v-list-item-content>
+      <v-list-item-title>
+        Convert to note
+      </v-list-item-title>
+    </v-list-item-content>
+    <v-list-item-icon>
+      <v-icon>
+        mdi-note
+      </v-icon>
+    </v-list-item-icon>
+  </v-list-item>
+</template>
+
+<script>
+import pouchMixin from "@/mixins/pouchMixin";
+
+export default {
+    name: 'ConvertTaskToNote',
+    mixins: [pouchMixin],
+    props: {
+        docid: {
+            type: String,
+            default: null,
+        }
+    },
+    methods: {
+        convert: async function() {
+      await this.setFieldGeneric({
+        _id: this.docid,
+        field: "type",
+        value: 'note'
+      });
+        },
+    }
+}
+</script>
