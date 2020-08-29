04446f55b039d70a1b28ed698ca32e6cc70d8569
lybekk <christoffer@lybekk.tech>
2020-08-29 03:51:19 +0200

    wip: feature: RSS reader

---
+++ src/components/documentviewer/DoctypeWebfeed.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-text-field
+        class="title"
+        label="Feed URL (RSS)"
+        :value="doc.url"
+        filled
+        @change="setDocField('url', $event)"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import pouchMixin from "@/mixins/pouchMixin";
+import itemDetailedMixin from "@/mixins/itemDetailedMixin";
+
+export default {
+  name: "DoctypeWebfeed",
+    mixins: [pouchMixin, itemDetailedMixin],
+    //mixins: [itemDetailedMixin],
+};
+</script>
